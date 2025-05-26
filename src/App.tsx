b9d95d594039fb8f6eb77dbb8c9fe83e43c07e60
nJosephu <nnyamahjosephng@gmail.com>
2025-05-26 12:58:27 +0100

    Clean up project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { BlogSection } from "./screens/SampleEcommerce/sections/BlogSection";
-import { CallToActionSection } from "./screens/SampleEcommerce/sections/CallToActionSection";
-import { FeaturesSection } from "./screens/SampleEcommerce/sections/FeaturesSection/FeaturesSection";
-import { FooterSection } from "./screens/SampleEcommerce/sections/FooterSection/FooterSection";
-import { ProductShowcaseSection } from "./screens/SampleEcommerce/sections/ProductShowcaseSection";
-import { ShopHeaderSection } from "./screens/SampleEcommerce/sections/ShopHeaderSection";
-import { TestimonialsSection } from "./screens/SampleEcommerce/sections/TestimonialsSection";
+import { BlogSection } from "./sections/BlogSection";
+import { CallToActionSection } from "./sections/CallToActionSection";
+import { FeaturesSection } from "./sections/FeaturesSection";
+import { FooterSection } from "./sections/FooterSection";
+import { ProductShowcaseSection } from "./sections/ProductShowcaseSection";
+import { ShopHeaderSection } from "./sections/ShopHeaderSection";
+import { TestimonialsSection } from "./sections/TestimonialsSection";
 
 export const App = (): JSX.Element => {
   return (
